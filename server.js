47f159dc6d14fd20af2b60cdcba64e68e850da6b
Frank Rauen <home@Franks-MacBook-Air.local>
2020-01-07 18:22:10 -0700

    add database and server base code

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('JS is listening')
+const express = require('express');
+const morgan = require('morgan');
+const port = 3000;
+
+require('./config/database');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Express is Listening on port ${port}`);
+});

cd8e8f3d4a54bb53908feb998559e9a511015be6
Frank Rauen <home@Franks-MacBook-Air.local>
2020-01-13 17:34:32 -0700

    adds passport and login capabilities

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const port = 3000;
 
 require('./config/database');
+require('./config/passport');
 
 const indexRouter = require('./routes/index');
 const artistsRouter = require('./routes/artists');
@@ -18,6 +21,12 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
+app.use(session({
+    secret: 'Project2',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/artists', artistsRouter);
